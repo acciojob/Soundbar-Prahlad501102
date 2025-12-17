62d924f7810e7aa4e02657ebd516e88d1d9c7402
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-18 00:47:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-let audio;
+let audio = null;
 
-document.querySelectorAll('.btn').forEach(button => {
+// Play sound buttons
+document.querySelectorAll('.btn').forEach((button) => {
   button.addEventListener('click', () => {
     if (audio) {
       audio.pause();
       audio.currentTime = 0;
     }
+
     audio = new Audio(`sounds/${button.innerText}.mp3`);
     audio.play();
   });
 });
 
+// Stop button
 document.querySelector('.stop').addEventListener('click', () => {
   if (audio) {
     audio.pause();
