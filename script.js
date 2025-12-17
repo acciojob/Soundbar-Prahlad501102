63e09c5bd3c15919fd1e4847fa5aaf174410586a
PrahladKumar <prahladchoudhary7980@gmail.com>
2025-12-17 14:21:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+const btnContainer = document.getElementById('buttons');
+
+sounds.forEach(sound => {
+    const btn = document.querySelector(`.btn:nth-child(${sounds.indexOf(sound) + 1})`);
+
+    btn.addEventListener('click', () => {
+        stopSongs();
+        document.getElementById(sound).play();
+    });
+});
+
+document.querySelector('.stop').addEventListener('click', () => {
+    stopSongs();
+});
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound);
+        song.pause();
+        song.currentTime = 0;
+    });
+}
